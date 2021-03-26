a77ebcbda70b8ef82b06cfaf66d2b0a22f7a076b
CanDagdelen94 <candagdelenn@gmail.com>
2021-03-27 01:40:08 +0300

    Frontend Deneme

---
+++ Northwind-Frontend/src/app/components/cart-summary/cart-summary.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CartItem } from 'src/app/models/cartitem';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-summary',
+  templateUrl: './cart-summary.component.html',
+  styleUrls: ['./cart-summary.component.css']
+})
+export class CartSummaryComponent implements OnInit {
+
+  constructor(private cartService:CartService, private toastrService: ToastrService) { }
+  cartItems:CartItem[]=[];
+
+  ngOnInit(): void {
+    this.getCart();
+  }
+
+  getCart(){
+    this.cartItems = this.cartService.list()
+  }
+  removeFromCart(product: Product){
+    this.cartService.removeFromCart(product);
+    this.toastrService.error("Silindi", product.productName + " sepetten silindi.")
+  }
+}
---
+++ Northwind-Frontend/src/app/services/category.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Category } from '../models/category';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  apiUrl = "https://localhost:44380/api/categories/getall";
+  constructor(private httpClient:HttpClient) { }
+
+  getCategories():Observable<ListResponseModel<Category>> {
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
+  }
+}
